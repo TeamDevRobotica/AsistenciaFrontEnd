9ef7b5124ab8e705c1e0606f07b093fa5847a14a
TeamDevRobotica <jose.flores1087@gmail.com>
2020-04-03 20:29:06 -0300

    entrada, salida, vista

--- src/app/salida/salida.page.ts
+++ src/app/salida/salida.page.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./salida.page.scss'],
 })
 export class SalidaPage implements OnInit {
-
-  constructor() { }
+  fecha: Date;
+  usuario: any;
+  monthShortNames = ['Ene', 'feb', 'mar', 'Abr', 'May' , 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic' ];
+  constructor() { 
+    this.usuario = JSON.parse(localStorage.getItem('usuarios'));
+  }
 
   ngOnInit() {
   }
-
+  ionViewWillEnter(){
+    this.fecha = new Date();
+  };
 }
